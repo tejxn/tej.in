a64f80fabef63d5f093f73721a32179556784ab2
Tej <108144357+tejxn@users.noreply.github.com>
2026-02-03 22:29:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-404: Not Found
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.use(express.static(__dirname));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
